85cbfcf97079dd8ccd83699bf5399b85f607e872
Kromer Kromer <kerolloskromer@gmail.com>
2020-04-27 02:35:26 +0200

    fix pathes

--- Voicee/settings.py
+++ Voicee/settings.py
@@ -128,11 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(THIS_FOLDER, "static")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(THIS_FOLDER, "media")
 
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 FIREBASE_KEY = os.path.join(
     THIS_FOLDER, 'voicee-8e33b-firebase-adminsdk-575q2-223ca4a969.json')
